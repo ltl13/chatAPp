02ef4a53c8e6d4257fa338515a20eb7d61e92ec0
ltl13 <66907671+ltl13@users.noreply.github.com>
2021-08-06 15:05:29 +0700

    init project, add constants

---
+++ chat_app/lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// Màu chủ đạo
+const myWhiteColor = Color(0xfffbfbfb);
+const myPurpleColor = Color(0xff5b50dc);
+const myBlackColor = Color(0xff222540);
+const myGrayColor = Color(0xffacacac);
+const myDefaultPadding = 20; // Padding mặc định
+// Cái này là gradient, dùng cho text block trong chat screen
+const myPrimaryGradient = LinearGradient(
+  colors: [Color(0xff655ae4), Color(0xff463bc7)],
+  begin: Alignment.topCenter,
+  end: Alignment.bottomCenter,
+);
---
+++ chat_app/lib/views/friends_list/friends_list_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FriendListScreen extends StatefulWidget {
+  const FriendListScreen({Key key}) : super(key: key);
+
+  @override
+  _FriendListScreenState createState() => _FriendListScreenState();
+}
+
+class _FriendListScreenState extends State<FriendListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(actions: [],),
+    );
+  }
+}
